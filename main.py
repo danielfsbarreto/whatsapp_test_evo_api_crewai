dcf3e2190d52d57200918b3dd3ea07f7a11715fe
Daniel Barreto <danielfsbarreto@gmail.com>
2025-09-09 15:08:35 -0300

    Add uvicorn host and port

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
@@ -19,3 +22,9 @@ async def messages_upsert(body: WhatsappRequestBody):
     print(body.model_dump())
     MessageSubmissionService().kickoff_interaction(body)
     return body.model_dump()
+
+
+if __name__ == "__main__":
+    host = os.getenv("UVICORN_HOST", "0.0.0.0")
+    port = int(os.getenv("UVICORN_PORT", "8000"))
+    uvicorn.run(app, host=host, port=port)
