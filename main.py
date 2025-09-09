53e08f7c92b2665177bb57a1be8b07b73f1ece1a
Daniel Barreto <danielfsbarreto@gmail.com>
2025-09-09 16:02:55 -0300

    Add API key on interaction between Evolution and FastAPI

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from models import WhatsappRequestBody
 from services import MessageSubmissionService
@@ -10,6 +11,17 @@ from services import MessageSubmissionService
 load_dotenv()
 
 app = FastAPI()
+security = HTTPBearer()
+
+
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    if credentials.credentials != os.getenv("UVICORN_API_KEY"):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return credentials.credentials
 
 
 @app.api_route("/", methods=["GET", "HEAD"])
@@ -18,7 +30,9 @@ async def root():
 
 
 @app.post("/messages-upsert")
-async def messages_upsert(body: WhatsappRequestBody):
+async def messages_upsert(
+    body: WhatsappRequestBody, token: str = Depends(verify_token)
+):
     print(body.model_dump())
     MessageSubmissionService().kickoff_interaction(body)
     return body.model_dump()
